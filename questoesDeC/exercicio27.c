134d49d939b98309bdad5ebaa9f896756990cf00
EzequielCosta <62070653+EzequielCosta@users.noreply.github.com>
2020-12-14 16:32:19 -0300

    Add files via upload

--- questoesDeC/exercicio27.c
+++ questoesDeC/exercicio27.c
@@ -1,55 +1,77 @@
 #include <stdio.h>
 
-main(){
+int main(){
 
     int sexo = 0;
     int idade = 0;
     int estado_civil = 0;
-    double media_idade_mulheres = 0, media_idade_homens = 0;
-    double percentual_homens_solteiros = 0, percentual_mulheres_solteiras = 0;
+    int quantidade_total_mulheres = 0, quantidade_total_homens = 0;
+    double percentual_mulheres_solteiras, percentual_homens_solteiros;
     int quantidade_mulheres_divorciadas = 0;
+    int quantidade_homens = 0 , quantidade_mulheres = 0;
+    int soma_idade_mulheres = 0, soma_idade_homens = 0;
+    int quantidade_homens_solteiros = 0, quantidade_mulheres_solteiras = 0 ;
     
-    while (1)
-    {
-        printf("Digite o sexo: ");
-        scanf("%s",&sexo); 
+
+
+    // media idade mulheres = (total_idades) / quantidade_total_mulheres ;
+    // media idade homens = (total_idades) / quantidade_total_homens ;
+    // percentual homens solteiros = ((quantidade_homens_solteiros) / quantidade_total_homens) * 100 ;
+    // percentual mulheres solteiros = ((quantidade_mulheres_solteiras) / quantidade_total_mulheres) * 100 ;
+    
+    for (int i = 1; i <= 3; i++){
+
+        printf("Digite o sexo (1=Masculino, 2=Feminino): ");
+        scanf("%d",&sexo); 
 
         printf("Digite a idade: ");
-        scanf("%s",&idade); 
+        scanf("%d",&idade); 
 
-        printf("Digite o estado civil: ");
-        scanf("%s",&estado_civil);
+        printf("Digite o estado civil (1=Casado, 2=Solteiro, 3=Divorciado, 4=Viuvo): ");
+        scanf("%d",&estado_civil);
+        printf("\n");
 
         if (sexo == 2){
-            media_idade_mulheres++;
-            if (estado_civil == 2){
-                percentual_mulheres_solteiras++; 
+            quantidade_total_mulheres++;
+            if (estado_civil == 2){ // se for solteira
+                quantidade_mulheres_solteiras++; 
             }
-            if (estado_civil == 3 && idade > 30){
+            if (estado_civil == 3 && idade > 30){ // se for divorciada
                 quantidade_mulheres_divorciadas++;
             }
+            soma_idade_mulheres += idade;
             
-            
-
-
         }else if(sexo == 1){
-            media_idade_homens++;
-            if (estado_civil == 2){
-                percentual_homens_solteiros++;
+            quantidade_total_homens++;
+            if (estado_civil == 2){ // se for solteiro
+                quantidade_homens_solteiros++;
             }
+
+            soma_idade_homens += idade;
             
         }
         
-        media_idade_homens = media_idade_homens / 100;
-        media_idade_mulheres = media_idade_mulheres / 100;
-        //percentual_homens_solteiros = (percentual_homens_solteiros / 100) * 100;
+       
+        
         //percentual_mulheres_solteiras = (percentual_mulheres_solteiras / 100) * 100;        
     }
+    printf("%d\n",quantidade_mulheres_solteiras);
+    printf("%d\n",quantidade_total_mulheres);
+    //printf("%d\n",quantidade_total_homens);
+    printf("\n");
+    //printf("%d\n",quantidade_total_homens);
 
-    printf("A media da idade dos homens eh: %f\n",media_idade_homens);
-    printf("A media da idade dos mulheres eh: %f\n",media_idade_mulheres);    
-    printf("O percentual de homens solteiros eh: %f\n",percentual_homens_solteiros);
-    printf("O percentual de mulheres solteiros eh: %f\n",percentual_mulheres_solteiras);
-    printf("A quantidade de mulheres divorciadas eh: %f",quantidade_mulheres_divorciadas);
+    double media_idade_mulheres = ( quantidade_total_mulheres != 0 ? soma_idade_mulheres / quantidade_total_mulheres : 0 );
+    double media_idade_homens = ( quantidade_total_homens != 0 ? soma_idade_homens / quantidade_total_homens : 0 );
+    percentual_homens_solteiros = (quantidade_total_homens !=0 ? ((double) quantidade_homens_solteiros / quantidade_total_homens) * 100 : 0);
+    percentual_mulheres_solteiras = (quantidade_total_mulheres !=0 ? ((double) quantidade_mulheres_solteiras / quantidade_total_mulheres) * 100 : 0);
+    
+    printf("A media da idade dos homens eh: %2.f\n",media_idade_homens);
+    printf("A media da idade das mulheres eh: %2.f\n",media_idade_mulheres);    
+    printf("O percentual de homens solteiros eh: %2.f\n",percentual_homens_solteiros);
+    printf("O percentual de mulheres solteiras eh: %2.f\n",percentual_mulheres_solteiras);
+    printf("A quantidade de mulheres divorciadas eh: %d",quantidade_mulheres_divorciadas);
+
+    return 0;
        
 }
